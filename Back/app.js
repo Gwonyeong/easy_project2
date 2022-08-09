92fb0f565c850506cb753752501460f7d0d03bb9
junsu1220 <crab7157@gmail.com>
2022-08-10 01:17:34 +0900

    연결 미약적으로 성공

--- Back/app.js
+++ Back/app.js
@@ -1,35 +1,29 @@
-const express = require("express")
+const express = require("express");
 
-const indexRouter = require("./routes")
-const {error404, error} = require("./middlewares/error")
+const indexRouter = require("./routes");
+const { error404, error } = require("./middlewares/error");
+const cors = require("cors");
 const app = express();
 app.use(express.json);
 
 class App {
-    constructor(){
-        this.app = express();
-        this.setMiddleWare();
-        this.setRouter();
-        // this.setErrorHandler();
-        
-    }
-    setMiddleWare(){
-        
-        this.app.use(express.json());
-        this.app.use(express.urlencoded({ extended : false}));
-    }
-    setRouter(){
-        
-        
-        this.app.use("/", indexRouter);
-        
-    }
-    setErrorHandler(){
-        
-        this.app.use(error404);
-        this.app.use(error);
-    }
-   
-
+  constructor() {
+    this.app = express();
+    this.setMiddleWare();
+    this.setRouter();
+    // this.setErrorHandler();
+  }
+  setMiddleWare() {
+    this.app.use(cors());
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
+  }
+  setRouter() {
+    this.app.use("/", indexRouter);
+  }
+  setErrorHandler() {
+    this.app.use(error404);
+    this.app.use(error);
+  }
 }
-module.exports = new App().app
+module.exports = new App().app;
